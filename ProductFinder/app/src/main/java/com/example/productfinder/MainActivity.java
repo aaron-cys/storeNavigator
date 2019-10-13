0e938753dd52cd0ea271a022fe8b913033bf81d6
s3785534 <54345702+s3785534@users.noreply.github.com>
2019-10-13 18:10:27 +1100

    Added Google Maps Activity
    Maps activity added. Slight colour and icon changes.

--- ProductFinder/app/src/main/java/com/example/productfinder/MainActivity.java
+++ ProductFinder/app/src/main/java/com/example/productfinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.productfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -25,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
+
     }
 
     @Override
