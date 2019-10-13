292e9b6352d0053551d5ae85a622d90daa7c85ad
s3785534 <54345702+s3785534@users.noreply.github.com>
2019-10-13 16:50:20 +1100

    Basic Packages

---
+++ ProductFinder/app/src/main/java/com/example/productfinder/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.productfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#EDF1FF"))
+                .withHeaderText("Product Finder")
+                .withFooterText("TR-3")
+                .withBeforeLogoText("Text")
+                .withAfterLogoText("Text")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View SplashScreen = config.create();
+        setContentView(SplashScreen);
+
+
+    }
+}
